97c1f1d30ee788ad9b22a07615606de614fb8f5c
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-13 12:19:41 +0200

    linking components done

--- src/Layout/SideNav.js
+++ src/Layout/SideNav.js
@@ -1,20 +1,17 @@
 /** @format */
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SideNav = () => {
 	return (
 		<div className="bg-blue-300 my-1 px-2 w-full overflow-hidden sm:my-1 sm:px-1 md:my-1 md:px-1 md:w-1/4 lg:my-px lg:px-px lg:w-1/4 xl:my-px xl:px-px xl:w-1/4">
 			<ul className="px-5 py-4">
 				<li className=" px-2">
-					<a className="text-xl py-4" href="#home">
-						Home
-					</a>
+					<Link to="/" className="text-xl py-4">Home</Link>
 				</li>
 				<li className=" px-2">
-					<a className="text-xl py-4" href="#home">
-						Cart
-					</a>
+					<Link to="/cart" className="text-xl py-4">Cart</Link>
 				</li>
 			</ul>
 		</div>
