ae9061f664e1d1cdd00f4f732ed55a2d38e70e62
PrinceNiyonshuti <npprince47@gmail.com>
2021-11-13 16:08:45 +0200

    changed trash to red color

--- src/Components/CartProducts.js
+++ src/Components/CartProducts.js
@@ -34,7 +34,7 @@ const CartProducts = ({ cartData, RemoveItem }) => {
 								className="h-6 w-6"
 								fill="none"
 								viewBox="0 0 24 24"
-								stroke="currentColor">
+								stroke="red">
 								<path
 									strokeLinecap="round"
 									strokeLinejoin="round"
